8ade8a3bfa2b8f133041376ebc0c017e3b804a46
jurajzachar <juraj.zachar@gmail.com>
2016-11-28 02:13:38 +0100

    fix js formatting

--- app/assets/js/main.js
+++ app/assets/js/main.js
@@ -1,34 +1,34 @@
 // `main.js` is the file that sbt-web will use as an entry point
 (function(requirejs) {
-	'use strict';
+  'use strict';
 
-	// -- RequireJS config --
-	requirejs.config({
-		shim : {
-			'jsRoutes' : {
-				deps : [],
-				// it's not a RequireJS module, so we have to tell it what var
-				// is returned
-				exports : 'jsRoutes'
-			},
-			'bootstrap' : [ 'jquery' ]
-		},
-		paths : {
-			'requirejs' : [ '../lib/requirejs/require' ],
-			'jquery' : [ '../lib/jquery/jquery' ],
-			'bootstrap' : [ '../lib/bootstrap/js/bootstrap' ],
-			'vue' : [ '../lib/vue/vue' ],
-			'jsRoutes' : [ '/jsroutes' ],
-			//net utils for loading templates
-			'net' : ['./utils/net'],
-			'app' : [ './app' ]
-		}
-	});
+  // -- RequireJS config --
+  requirejs.config({
+    shim : {
+      'jsRoutes' : {
+        deps : [],
+        // it's not a RequireJS module, so we have to tell it what var
+        // is returned
+        exports : 'jsRoutes'
+      },
+      'bootstrap' : [ 'jquery' ]
+    },
+    paths : {
+      'requirejs' : [ '../lib/requirejs/require' ],
+      'jquery' : [ '../lib/jquery/jquery' ],
+      'bootstrap' : [ '../lib/bootstrap/js/bootstrap' ],
+      'vue' : [ '../lib/vue/vue' ],
+      'jsRoutes' : [ '/jsroutes' ],
+      // net utils for loading templates
+      'net' : [ './utils/net' ],
+      'app' : [ './app' ]
+    }
+  });
 
-	requirejs.onError = function(err) {
-		console.log(err);
-	};
+  requirejs.onError = function(err) {
+    console.log(err);
+  };
 
-	// Load the app. This is kept minimal so it doesn't need much updating.
-	require([ 'app' ]);
+  // Load the app. This is kept minimal so it doesn't need much updating.
+  require([ 'app' ]);
 })(requirejs);
